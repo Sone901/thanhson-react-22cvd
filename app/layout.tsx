b3be689db2be6f46959f92aa3e1dafe046dc4b4d
Sone901 <jtr.secret.901@gmail.com>
2026-01-28 22:50:11 +0700

    chore: optimize - remove unused fonts and CSS variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Portfolio',
+  description: 'Personal Information  of Tran Thanh Son',
+  generator: ' Next.js  13.4',
 }
 
 export default function RootLayout({
